19f4254b394b8eeaff10c9613ded6f192634183e
Dominic Fatonade <dfatonade@gmail.com>
2026-07-07 06:26:19 +0000

    Added space optimal solution for reorder list problem. Applied a little formatting to other solutions.

--- Linked-Lists/reorder_list.py
+++ Linked-Lists/reorder_list.py
@@ -4,6 +4,7 @@
 #         self.val = val
 #         self.next = next
 
+
 # Space heavy but easier to maintain solution
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
@@ -39,4 +40,45 @@ class Solution:
 """
 Time complexity - O(n)
 Space complexity - O(n)
+"""
+
+
+# Space optimal but trickier solution
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        #find halfway point of list
+        slow, fast = head, head.next
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        p2 = slow.next
+        slow.next = None
+
+        #reverse second half
+        prev = None
+        
+        while p2:
+            nxt = p2.next
+            p2.next = prev
+            prev = p2
+            p2 = nxt
+
+        #merge two halves
+        p1, p2 = head, prev
+
+        while p2:
+            p1_nxt = p1.next
+            p2_nxt = p2.next
+
+            p1.next = p2
+            p2.next = p1_nxt
+
+"""
+Time complexity - O(n)
+Space complexity - O(1)
 """
